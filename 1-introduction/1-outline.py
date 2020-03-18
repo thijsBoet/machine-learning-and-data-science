383e5441e1620b3c423233073b20b56e5a730f65
Matthijs Boet <m.boet@chello.nl>
2020-03-18 05:34:01 +0100

    created outline for course

---
+++ 1-introduction/1-outline.py
@@ -0,0 +1,10 @@
+# 1 - machine learning 101
+# 2 - work environment
+# 3 - data analysis
+# 4 - numpy
+# 5 - data visualisation (matplotlib)
+# 6 - Scikit-learn
+# 7 - Supervised learning (classification, regression)
+# 8 - Neural Networks (transfer learning, deep learning, time series)
+# 9 - Data Enginering
+# 10 - Storytelling & Communication
